8443837f48856e870bc0a2dbd9f009606ca706fd
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-03-09 17:46:19 +0100

    Remove missing columns from training dataset

--- train.py
+++ train.py
@@ -17,10 +17,44 @@ def load_csv(filename):
 
     return data[1:]
 
+'''
+@breif finds the missing columns in the dataset, missing column is defined as when the len of data is zero
+
+@param data the dataset
+
+@return missing_columns An reverse sorted array with unique indexs of missing columns
+'''
+def find_missing_columns(data):
+    missing_columns = [] #clear the missing column storage
+    for line in data:
+        for index, data_point in enumerate(line):
+            if(len(data_point) == 0): #the column is missing
+                missing_columns.append(index)
+
+    return sorted(set(missing_columns), reverse=True)
+
+'''
+@breif Removes the columns from the dataset
+
+@param data the dataset to delete from
+@param columns an reverse sorted list of columns to be deleted
+
+@return data the new dataset where columns are removed
+'''
+def remove_columns(data, columns):
+    edited_data = []
+    for line in data:
+        for column in columns:
+            del line[column]
+        edited_data.append(line)
+
+    return edited_data
+
 
 if __name__ == '__main__':
     trainingData = load_csv('dataset/train.csv')
-    
-    t1 = tree.Tree()
+
+    missing_columns = find_missing_columns(trainingData)
+    trainingData = remove_columns(trainingData, missing_columns)
 
     print(trainingData[0])
